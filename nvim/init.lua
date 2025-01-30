97f26aa693f46197b1a753d186ffc2dde8e33310
Priyansh Singh <priyanshsingh@outlook.com>
2025-01-30 19:50:11 +0530

    ..

--- nvim/init.lua
+++ nvim/init.lua
@@ -2,5 +2,5 @@ require("boardslayer")
 require("boardslayer.lazy")
 require("boardslayer.plugins")
 -- vim.cmd("colorscheme rose-pine")
-vim.cmd("colorscheme rose-pine-dawn")
+vim.cmd("colorscheme rose-pine-moon")
 -- vim.cm
